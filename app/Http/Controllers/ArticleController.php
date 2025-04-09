f59bf8e7a65fecc885f2389aaa0d10e54292d0be
zyk0 <77777mill@gmail.com>
2025-04-09 21:40:47 +0500

    bugfix

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,7 +6,25 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+	/*
     public function index(){
         return view('article.index');
     }
+	*/
+	    public function index(){
+		
+		$firstid = Product::first('id', 'asc')->first();		
+		$lastid =  Product::latest('id', 'desc')->first();
+
+		$randomproduct = rand($firstid->id, $lastid->id); //рандомный id продукта из таблицы
+        $products = Product::where('id', $randomproduct)->first();
+		
+		$oneproduct = $products;
+		
+		return view('article.index',[
+            'oneproduct' => $oneproduct,
+			'firstid' => $firstid->id,
+			'lastid' => $lastid->id,
+        ]);
+		}
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -107,17 +107,34 @@
         <div class="container">
             <div class="row g-5 align-items-center">
                 <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                    <img class="img-fluid animated pulse infinite" src="img/shampoo-1.png">
+				
+					<?php
+						$​reflection = '';
+						if(count($oneproduct->images) > 0){
+							$​reflection = $oneproduct->images[0]['img'];
+						} else {
+							$​reflection = 'null.jpg';
+						}
+					?>
+					<img class="img-fluid" src="img/{{ $​reflection }}" alt="{{ $oneproduct->title }}">
+					<!-- <img class="img-fluid" src="img/{{ $oneproduct->images[0]['img']; }}"> -->
                 </div>
                 <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                    <h1 class="text-primary mb-4">Fern <span class="fw-light text-dark">is a Symbol of
-                            Beauty</span></h1>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non
-                        malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus. Praesent
-                        tristique odio ut rutrum pellentesque. Fusce eget molestie est, at rutrum est.</p>
-                    <p class="mb-4">Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no
-                        labore lorem sit. Sanctus clita duo justo et tempor.</p>
-                    <a class="btn btn-primary py-2 px-4" href="">Заказ</a>
+                    <h1 class="text-primary mb-4">Fern <span class="fw-light text-dark">is a Symbol of Beauty</span></h1>
+                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p class="mb-4">
+						<div class="row align-items-center">
+							<div class=" text-center text-lg-start">
+								<p class=" text-white  slideInRight">
+									<span class="fw-light text-dark"> Count: {{ $firstid }} - {{$lastid}} Fern</span>  
+								</p>
+								<p class="text-dark slideInRight fw-bold">{{ $oneproduct->id }} 
+								<span class="fw-light text-dark">{{$oneproduct->title}}</span> </p>
+								<p class="text-dark mb-4  slideInRight">{{$oneproduct->description}}</p>
+							</div>
+						</div>
+					</p>
+                    <a class="btn btn-primary py-2 px-4" href="#">Заказ</a>
                 </div>
             </div>
         </div>
